356b85c3f148a8e5f8189284227c45a3e5798412
adhikarisayanava <sayanava.adhikari19@gmail.com>
2024-05-07 16:13:22 +0200

    added example code for python decorator using wrapper

--- LearningCourse/Basics/PythonDecorator.py
+++ LearningCourse/Basics/PythonDecorator.py
@@ -1,19 +1,13 @@
-import time
-
-def timing_decorator(func):
-    def wrapper(time_count):
-        start = time.time() # get the current time
-        result = func(time_count) # call the original function
-        end = time.time() # get the current time again
-        print(f"Time taken by {func.__name__} function: {end - start} seconds")
-        return result
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+   
     return wrapper
 
-@timing_decorator
-def my_func(n):
-    sum = 0
-    for i in range(n):
-        sum += i
-    return sum
+@my_decorator
+def say_hi():
+    print("Hello, world!")
 
-my_func(1000000)
+say_hi()
